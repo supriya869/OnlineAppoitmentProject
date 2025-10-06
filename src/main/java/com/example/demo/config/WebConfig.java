b80cb0812763dc2d045f2e3ce7649cc8c0933c50
Supriya Nerella <ammu99367@gmail.com>
2025-10-06 18:10:40 +0530

    Create WebConfig.java

---
+++ src/main/java/com/example/demo/config/WebConfig.java
@@ -0,0 +1,28 @@
+package com.example.demo.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(
+                        		"http://localhost:3000",
+                        		"https://doctorbookingfrontend-git-main-supriya-nerellas-projects.vercel"
+                        		) // ✅ Replace with your Vercel frontend URL
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
